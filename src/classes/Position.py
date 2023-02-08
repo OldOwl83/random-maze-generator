ce5b175c2cf39629961bf3631b8acfeb08f09ab5
Mauro Donnantuoni Moratto <mauroariel.donnantuonimoratto@everis.nttdata.com>
2023-02-08 17:58:40 -0300

    Mejor encapsulamiento del atrib. 'paths' de Position

--- src/classes/Position.py
+++ src/classes/Position.py
@@ -2,19 +2,25 @@ class Position:
     def __init__(self) -> None:
         self._paths = []
 
+
+    # Control del atributo paths
     def get_paths(self):
         return self._paths
     
-    def set_paths(self, *args):
+    def set_paths(self, value):
         raise PermissionError(
             'No se puede alterar este atributo directamente. Utilice los '
             'métodos "add_path" y "rm_path" en su lugar.'
         )
+
+    def del_paths(self):
+        self._paths = []
     
-    paths = property(get_paths, set_paths)
+    paths = property(get_paths, set_paths, del_paths)
+
 
     def __bool__(self):
-        return bool(self._paths)
+        return bool(self.paths)
     
 
     def __str__(self):
@@ -25,7 +31,7 @@ class Position:
             '-'.join([
             (f'{p["path"]}'
              f'{"".join([unicode_subins[int(d)] for d in str(p["order"])])}')
-            for p in self._paths])
+            for p in self.paths])
         )
             
 
@@ -38,7 +44,7 @@ class Position:
                 'Los parámetros "path" y "order" deben ser números naturales o 0.'
             )
         
-        for p in self._paths:
+        for p in self.paths:
             if path == p['path']:
                 raise ValueError(
                     'Esta "Position" ya tiene asignado un "order" '
@@ -50,7 +56,7 @@ class Position:
                     'Una "Position" sólo puede tener un "order" distinto de 0.'
                 )
 
-        self._paths.append(
+        self.paths.append(
             {
                 'path': path,
                 'order': order
@@ -60,9 +66,9 @@ class Position:
     def rm_path(self, path):
         rm_flag = False
 
-        for p in self._paths:
+        for p in self.paths:
             if p['path'] == path:
-                self._paths.remove(p)
+                self.paths.remove(p)
                 rm_flag = True
 
         if not rm_flag:
@@ -72,8 +78,8 @@ class Position:
 
 if __name__ == '__main__':
     p1 = Position()
-    #rint(bool(p1))
+    
     p1.add_path(1, 23)
     p1.add_path(3, 0)
-    #p1.rm_path(3)
-    print((p1))
+    
+    print((p1.paths))
