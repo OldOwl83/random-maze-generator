bc9ef025f0c75f31c5a9b88bc8e2065eae7a6d63
Sebadgc <sebastiandegese@gmail.com>
2023-02-18 18:35:06 -0300

    changes to .gitignore

--- utils/constants.py
+++ utils/constants.py
@@ -1,8 +1,8 @@
 # Dimensions of the maze
-WIDTH = 75
-HEIGHT = 75
+WIDTH = 80
+HEIGHT = 80
 
 #str Directory with wall sprites
 WALL_PATH = r'C:\Users\Seba\Desktop\Maze\random-maze-generator\walls\\'
 
-SAVE_PATH = r'C:\Users\Seba\Desktop\\'
+SAVE_PATH = r'C:\Users\Seba\Desktop\Maze\random-maze-generator\mazes\\'
